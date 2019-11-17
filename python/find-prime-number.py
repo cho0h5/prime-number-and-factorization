5dcbe1e370a14687fccd59df91d7d36efe167139
Youngho Cho <dominoyh5@gmail.com>
2019-11-17 13:32:24 +0900

    complete prime

--- python/find-prime-number.py
+++ python/find-prime-number.py
@@ -1,23 +1,24 @@
 import sys
 
-#limit = int(sys.argv[1])
-limit = 10
+limit = int(sys.argv[1])
+#limit = 10
 
 count = 1
 primeList = [2]
-print(2)
+print(primeList)
 i = 3
+flag = True
 
-while True:
-    if count < limit:
-        print(primeList)
-        for j in primeList:
-            if (i % j) == 0:
-                break
-            primeList.append(i)
-            print(i)
-            count += 1
+while count < limit:
+    for j in primeList:
+        if (i % j) == 0:
+            flag = False
             break
-        i += 1
-    else:
-        break
+
+    if flag:
+        primeList.append(i)
+        print(primeList)
+        count += 1
+        
+    i += 2
+    flag = True
