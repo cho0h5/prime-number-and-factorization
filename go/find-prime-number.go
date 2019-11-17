aed18e8a567ff8893d4a78d31c5b457ed4b48b35
Youngho Cho <dominoyh5@gmail.com>
2019-11-17 14:55:01 +0900

    create go file

---
+++ go/find-prime-number.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	limit, _ := strconv.Atoi(os.Args[1])
+
+	count := 1
+	primeList := []int{2}
+	i := 3
+	flag := true
+
+	startTime := time.Now()
+
+	for count < limit {
+		for j := range primeList {
+			if (i % j) == 0 {
+				flag = false
+				break
+			}
+		}
+
+		if flag {
+			primeList = append(primeList, i)
+			count += 1
+		}
+
+		i += 2
+		flag = true
+	}
+
+	fmt.Println("time :", time.Since(startTime))
+	fmt.Println(primeList[len(primeList)-1])
+
+}
