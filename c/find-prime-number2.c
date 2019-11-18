75c775c9c4f49bee003eae0d2521b4a035c35216
Youngho Cho <dominoyh5@gmail.com>
2019-11-18 14:30:45 +0900

    update c 2 file

--- c/find-prime-number2.c
+++ c/find-prime-number2.c
@@ -2,15 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define SIZE 1000000
+
 int main(int argc, char *argv[]) {
 
-    //long limit = atol(argv[1]);
+    long limit = atol(argv[1]);
 
-    int primeList[100000] = {2};
+    int count = 1;
+    int primeList[SIZE] = {1};
+    primeList[0] = 0;
+    primeList[1] = 0;
+    int i = 3;
+    int flag = 1;
 
     clock_t startTime = clock();
 
-    
+    while(count < limit) {
+        int j;
+        for(j = count + 1; primeList[j] != 0; j++) {}
+        for(int k = 0; k < SIZE; k += j) {
+            primeList[k] = 0;
+	}
+
+    }
 
     printf("time : %f\n", (float)((clock() - startTime))/CLOCKS_PER_SEC);
     //printf("%d\n", primeList[count-1]);
